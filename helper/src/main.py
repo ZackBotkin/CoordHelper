899555d8e442bcf1f8b379dc431a5aea74e6ce72
Zack Botkin <zackbotkin@gmail.com>
2024-05-17 12:16:13 -0500

    add configs and arg parsing

--- helper/src/main.py
+++ helper/src/main.py
@@ -1,9 +1,16 @@
 import argparse
+from helper.src.config.config import Configs
 from helper.src.context_manager import ContextManager
 from helper.src.interactive.main_menu import MainMenu
 
 def main():
-    context_manager = ContextManager({"line_start": ">>>"})
+
+    parser = argparse.ArgumentParser(description= 'default parser')
+    parser.add_argument('--config_file', help='the configuration file')
+    args = parser.parse_args()
+
+    configs = Configs(args.config_file)
+    context_manager = ContextManager(configs)
     menu = MainMenu(context_manager)
     menu.main_loop()
 
